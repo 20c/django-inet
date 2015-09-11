53326d00a622dfc896a3f2331a57fed58702a8aa
Stefan Pratter <stefan@20c.com>
2015-09-11 12:29:16 +0000

    impl IPPrefixField for rest serializer

--- django_inet/rest.py
+++ django_inet/rest.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from models import IPAddressValidator, ValidationError
+from models import IPAddressValidator, IPPrefixValidator, ValidationError
 
 class IPAddressField(serializers.CharField):
     version = 0
@@ -10,3 +10,12 @@ class IPAddressField(serializers.CharField):
             del kwargs["version"]
         super(IPAddressField, self).__init__(**kwargs)
         self.validators.append(IPAddressValidator(self))
+
+class IPPrefixField(serializers.CharField):
+    version = 0
+    def __init__(self, **kwargs):
+        self.version = kwargs.get("version", 0)
+        if "version" in kwargs:
+            del kwargs["version"]
+        super(IPPrefixField, self).__init__(**kwargs)
+        self.validators.append(IPPrefixValidator(self))
